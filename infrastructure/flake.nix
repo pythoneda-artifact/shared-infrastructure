9e02f565280dbf087bd487cae009f80f1fa09116
rydnr <github@acm-sl.org>
2023-07-25 10:07:18 +0200

    Removed invalid input

--- infrastructure/flake.nix
+++ infrastructure/flake.nix
@@ -3,13 +3,6 @@
   inputs = rec {
     nixos.url = "github:NixOS/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils/v1.0.0";
-    pythoneda-realm-rydnr-events = {
-      url = "github:pythoneda-realm-rydnr/events-artifact/0.0.1a1?dir=events";
-      inputs.nixos.follows = "nixos";
-      inputs.flake-utils.follows = "flake-utils";
-      inputs.pythoneda-shared-pythoneda-domain.follows =
-        "pythoneda-shared-pythoneda-domain";
-    };
     pythoneda-shared-pythoneda-domain = {
       url =
         "github:pythoneda-shared-pythoneda/domain-artifact/0.0.1a25?dir=domain";
